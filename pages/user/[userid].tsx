56d4fd24c7b7cd702864cbfa3255dc30b057dbb1
Emmanuel Kyeremeh <josephkyeremeh53@gmail.com>
2021-05-03 18:24:17 +0000

    Modified post map

--- pages/user/[userid].tsx
+++ pages/user/[userid].tsx
@@ -166,7 +166,7 @@ const UserDetail = () => {
                 className="post-hover-image"
                 width="290"
                 height="300"
-                src={`/${post.image}`}
+                src={`/${post.actualImage}`}
                 onClick={(e) => router.push(`/post/${post._id}`)}
               />
             </div>
